344cba8f21570b49ea57a870cb4d274d730c6a5e
dyuvaraj65 <35591207+dyuvaraj65@users.noreply.github.com>
2020-05-01 15:20:45 -0500

    Update SumofNaturalNumbers.ps1

--- SumofNaturalNumbers.ps1
+++ SumofNaturalNumbers.ps1
@@ -24,4 +24,6 @@ $sum = (Get-Sum $n $x) + (Get-Sum $n $y) - (Get-Sum $n ($x*$y))
  
 #Find the sum of all the natural numbers up to $n which are divisible by $x or $y
 Write-Host "The sum of natural numbers below $($n) which are divisible by $($x) or $($y): $($sum)" 
+
+#Output: The sum of natural numbers below 1000 which are divisible by 3 or 5: 233168
  
